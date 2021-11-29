2b2c50fe98292853326b4d671e0d73f1a5efbaa3
wilderbit <abrar.nitk@gmail.com>
2021-11-29 17:42:50 +0530

    read node id from env file

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,31 @@ async fn health() -> impl Responder {
 }
 
 lazy_static::lazy_static!{
-    static ref ID: std::sync::Mutex<id_generator::UniqueIdGen> = std::sync::Mutex::new(id_generator::UniqueIdGen::new(1));
+    static ref ID: std::sync::Mutex<Option<id_generator::UniqueIdGen>> = std::sync::Mutex::new(None);
 }
 
 async fn id() -> impl Responder {
-    HttpResponse::Ok().body(serde_json::json!({"id": ID.lock().unwrap().next_id()}))
+    match ID.lock().unwrap().as_mut() {
+        Some(ref mut id) => {
+            HttpResponse::Ok().body(serde_json::json!({"id": id.next_id(), "success": true}))
+        }
+        None => {
+            HttpResponse::InternalServerError().body(serde_json::json!({"success": false, "message": "Something went wrong"}))
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    // std::env::set_var("RUST_LOG", "actix_web=trace,actix_redis=trace");
-    // env_logger::init();
-    // dotenv::dotenv().expect("Failed to load .env file");
+    std::env::set_var("RUST_LOG", "actix_web=trace,actix_redis=trace");
+    env_logger::init();
+    dotenv::dotenv().expect("Failed to load .env file");
+    let node_id: u16 = std::env::var("NODE_ID").unwrap().parse().map_err(|_e| std::io::ErrorKind::NotFound)?;
 
-    // let node_id: u16 = std::env::var("NODE_ID").unwrap().parse().map_err(|_e| std::io::ErrorKind::NotFound)?;
+    let mut builder = ID.lock().expect("error while acquiring lock");
+    *builder = Some(id_generator::UniqueIdGen::new(node_id));
+    std::mem::drop(builder);
 
     HttpServer::new(move || {
         App::new()
