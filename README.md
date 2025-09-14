66aa637718e5aef9c9b8ece0685e8f45968b9dc5
AbrarNitk <abrar.nitk@gmail.com>
2025-09-14 22:54:42 +0530

    start refactor

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 This repository contains code related to unique ID generator written in Rust.
 
 
+## Next Achievements
+
+- [] separate the service from library and use hyper to serve or axum
+- [] removal of the lazy_static at all
+- [] lock free concurrency, usage of the Atomic Primitive
+- [] Divide the node-address in to the data-center and the machine-id
+- [] node-address can be fallback to ip-address if not provided
+
+
+
+## Snowflake ID Structure
+
+- From left to right bits order: 64 bits
+
+- 1 Bit: unused bit, keeping as 0
+- 41 Bits: timestamp in millis
+- 5 Bits: Data center ID
+- 5 Bits: Machine ID
+- 12 Bits: Sequence(Generated atomically within timestamp)
+
+
+
+## References
+
+- [X Twitter Blog](https://blog.x.com/engineering/en_us/a/2010/announcing-snowflake)
--- src/main.rs
+++ src/main.rs
@@ -9,58 +9,62 @@ use actix_web::{
     web,
 };
 
-extern crate id_generator;
+// extern crate id_generator;
 
-async fn health() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!: ".to_string() + &NODE_ID.read().unwrap().to_string())
-}
+// async fn health() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!: ".to_string() + &NODE_ID.read().unwrap().to_string())
+// }
 
-lazy_static::lazy_static! {
-    static ref ID: std::sync::Mutex<Option<id_generator::UniqueIdGen>> = std::sync::Mutex::new(None);
-}
-
-lazy_static::lazy_static! {
-    static ref NODE_ID: std::sync::RwLock<u16> = std::sync::RwLock::new(0);
-}
+// lazy_static::lazy_static! {
+//     static ref ID: std::sync::Mutex<Option<id_generator::UniqueIdGen>> = std::sync::Mutex::new(None);
+// }
+//
+// lazy_static::lazy_static! {
+//     static ref NODE_ID: std::sync::RwLock<u16> = std::sync::RwLock::new(0);
+// }
 
 async fn id() -> impl Responder {
-    match ID.lock().unwrap().as_mut() {
-        Some(ref mut id) => {
-            HttpResponse::Ok().body(serde_json::json!({"id": id.next_id(), "success": true}))
-        }
-        None => HttpResponse::InternalServerError()
-            .body(serde_json::json!({"success": false, "message": "Something went wrong"})),
-    }
+
+    HttpResponse::InternalServerError()
+        .body(serde_json::json!({"success": false, "message": "Something went wrong"}))
+
+    // match ID.lock().unwrap().as_mut() {
+    //     Some(ref mut id) => {
+    //         HttpResponse::Ok().body(serde_json::json!({"id": id.next_id(), "success": true}))
+    //     }
+    //     None => HttpResponse::InternalServerError()
+    //         .body(serde_json::json!({"success": false, "message": "Something went wrong"})),
+    // }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // std::env::set_var("RUST_LOG", "actix_web=trace,actix_redis=trace");
-    env_logger::init();
-    dotenv::dotenv().expect("Failed to load .env file");
-    let node_id: u16 = std::env::var("NODE_ID")
-        .unwrap()
-        .parse()
-        .map_err(|_e| std::io::ErrorKind::NotFound)?;
-
-    {
-        let mut n = NODE_ID.write().unwrap();
-        *n = node_id;
-    }
+    // env_logger::init();
+    // dotenv::dotenv().expect("Failed to load .env file");
+    // let node_id: u16 = std::env::var("NODE_ID")
+    //     .unwrap()
+    //     .parse()
+    //     .map_err(|_e| std::io::ErrorKind::NotFound)?;
+    //
+    // {
+    //     let mut n = NODE_ID.write().unwrap();
+    //     *n = node_id;
+    // }
 
     let port: u16 = std::env::var("PORT")
         .unwrap_or("9001".to_string())
         .parse()
         .map_err(|_e| std::io::ErrorKind::NotFound)?;
 
-    let mut builder = ID.lock().expect("error while acquiring lock");
-    *builder = Some(id_generator::UniqueIdGen::new(node_id));
-    std::mem::drop(builder);
+    // let mut builder = ID.lock().expect("error while acquiring lock");
+    // *builder = Some(id_generator::UniqueIdGen::new(node_id));
+    // std::mem::drop(builder);
 
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .route("/health", web::get().to(health))
+            // .route("/health", web::get().to(health))
             .route("/v1", web::get().to(id))
     })
     .bind(("0.0.0.0", port))?
